8648c32d348c114d4dc335f4e1e9279ca8c708e6
fs <xcorter@mail.ru>
2020-01-28 22:47:17 +0700

    init commit

---
+++ app/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
+
+	port, exists := os.LookupEnv("port")
+	if !exists {
+		port = "80"
+	}
+	http.ListenAndServe(":" + port, nil)
+}
